afb71363ba7e531ea21c78f3661565014df64dbc
liuyanliang <2214888601@qq.com>
2022-11-15 19:40:59 +0800

    1115

--- daniu/wx_jiekou/urls.py
+++ daniu/wx_jiekou/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     re_path(r'article', views.article, name='article'),
     re_path(r'categorys', views.categorys, name='categorys'),
-    re_path(r'info/(?P<id>\d+)', views.info, name='info')
 ]
