55851098a9c6160c901f1d80ea6d49e9e030e121
liuyanliang <2214888601@qq.com>
2022-11-17 15:32:32 +0800

    api完成

--- daniu/wx_jiekou/urls.py
+++ daniu/wx_jiekou/urls.py
@@ -1,15 +1,15 @@
 # 使用drf的视图集就不需要编写路由，通过DefaultRouter的register方法注册就可以了
 from os import path
-
 from django.urls import include, re_path
 from rest_framework.routers import DefaultRouter
-
 from . import views
 
 router = DefaultRouter()
-router.register('student', views.StudentView)
-router.register('course', views.CourseView)
-router.register('grade', views.GradeView)
+# router.register('student', views.StudentView)
+# router.register('course', views.CourseView)
+# router.register('grade', views.GradeView)
+router.register('category', views.CategoryView)
+router.register('article', views.ArticleView)
 
 urlpatterns = [
     re_path('', include(router.urls))
--- daniu/wx_jiekou/views.py
+++ daniu/wx_jiekou/views.py
@@ -1,19 +1,31 @@
 from rest_framework import viewsets
 
 from .models import Course, Grade, Student
-from .serializers import CourseSerializer, GradeSerializer, StudentSerializer
+# from .serializers import CourseSerializer, GradeSerializer, StudentSerializer, CategorySerializer
+from .serializers import CategorySerializer, ArticleSerializer
+from daniuapp.models import Category, Article
 
 
-class StudentView(viewsets.ModelViewSet):
-    queryset = Student.objects.all()  # 指定查询结果集
-    serializer_class = StudentSerializer  # 指定序列化器
+####文章分类序列化####
+class ArticleView(viewsets.ModelViewSet):
+    queryset = Article.objects.all()  # 指定查询结果集
+    serializer_class = ArticleSerializer  # 指定序列化器
+class CategoryView(viewsets.ModelViewSet):
+    queryset = Category.objects.all()  # 指定查询结果集
+    serializer_class = CategorySerializer  # 指定序列化器
+####文章分类序列化####
 
-
-class GradeView(viewsets.ModelViewSet):
-    queryset = Grade.objects.all()
-    serializer_class = GradeSerializer
-
-
-class CourseView(viewsets.ModelViewSet):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
+#
+# class StudentView(viewsets.ModelViewSet):
+#     queryset = Student.objects.all()  # 指定查询结果集
+#     serializer_class = StudentSerializer  # 指定序列化器
+#
+#
+# class GradeView(viewsets.ModelViewSet):
+#     queryset = Grade.objects.all()
+#     serializer_class = GradeSerializer
+#
+#
+# class CourseView(viewsets.ModelViewSet):
+#     queryset = Course.objects.all()
+#     serializer_class = CourseSerializer
